b24d7b6b083d3b0c47d5af8f30df34085a33ad5d
Buddhi Nadeeshan <Buddhinadeeshan97@gmail.com>
2024-07-13 19:13:38 +0530

    chart

---
+++ src/admin/menu-items/services.jsx
@@ -0,0 +1,48 @@
+// assets
+import InventoryIcon from "@mui/icons-material/Inventory";
+import AddchartIcon from '@mui/icons-material/Addchart';
+import QueryStatsIcon from '@mui/icons-material/QueryStats';
+
+// icons
+const icons = {
+  InventoryIcon,
+  AddchartIcon,
+  QueryStatsIcon
+};
+
+// ==============================|| MENU ITEMS - DASHBOARD ||============================== //
+
+const product = {
+  id: "services",
+  title: "Services",
+  type: "group",
+  children: [
+    {
+      id: "Order & Reviews",
+      title: "Order & Reviews",
+      type: "item",
+      url: "/courier/order_reviews",
+      icon: icons.InventoryIcon,
+      breadcrumbs: false,
+    },
+    {
+      id: "orders",
+      title: "Orders",
+      type: "item",
+      url: "/courier/add_product",
+      icon: icons.AddchartIcon,
+      breadcrumbs: false,
+    },
+
+    {
+      id: "order-statistics",
+      title: "Order Statistics",
+      type: "item",
+      url: "/courier/product_Statistics",
+      icon: icons.QueryStatsIcon,
+      breadcrumbs: false,
+    },
+  ],
+};
+
+export default product;
