4cbc2207b33deb82004d26ff75d44d28a41fce3b
Buddhi Nadeeshan <Buddhinadeeshan97@gmail.com>
2024-07-26 23:20:52 +0530

    modify primary header

--- src/public/components/PrimaryHeader.jsx
+++ src/public/components/PrimaryHeader.jsx
@@ -4,144 +4,154 @@ import {
   Typography,
   TextField,
   InputAdornment,
+  IconButton,
 } from "@mui/material";
 import React from "react";
 import Logo from "../assets/images/logo/Stylit_logo.png";
-import SearchIcon from "@mui/icons-material/Search";
-import { Link } from "react-router-dom";
-import ShopperSignUp from "../pages/shopper_sign_up/ShopperSignUp";
+import { Link, useNavigate } from "react-router-dom";
+import ProfileMenu from "./ProfileMenu";
+import "./primaryHeader.css";
 
-const PrimaryHeader = ({topHeaderColor}) => {
+const PrimaryHeader = ({ topHeaderColor }) => {
+  const navigate = useNavigate()
+
+  const navigateToSearchPage = () => {
+    navigate('/public/search')
+  }
   return (
-      <Stack direction="column" sx={{ borderBottom: "1px solid #f0f0f0" }}>
-        <Box
-          sx={{
-            backgroundColor: `${topHeaderColor}`,
-            height: "35px",
-          }}
-        />
+    <Stack direction="column" sx={{ borderBottom: "1px solid #f0f0f0" }}>
+      <Box
+        sx={{
+          backgroundColor: `${topHeaderColor}`,
+          height: "35px",
+        }}
+      />
+      <Stack
+        direction="row"
+        sx={{
+          position: "relative",
+          padding: "8px 25px",
+          display: "flex",
+          justifyContent: "space-around",
+          alignItems: "center",
+        }}
+      >
         <Stack
           direction="row"
+          spacing={5}
+          sx={{ position: "absolute", top: "30%", left: "8%" }}
+        >
+          <Link to="/public/women">
+            <Typography
+              variant="body2_nunito"
+              sx={{
+                "&:hover": {
+                  borderBottom: "3px solid black",
+                  paddingBottom: "16px",
+                },
+              }}
+            >
+              Women
+            </Typography>
+          </Link>
+          <Link to="/public/men">
+            <Typography
+              variant="body2_nunito"
+              sx={{
+                "&:hover": {
+                  borderBottom: "3px solid black",
+                  paddingBottom: "16px",
+                },
+              }}
+            >
+              Men
+            </Typography>
+          </Link>
+          <Link to="#">
+            <Typography
+              variant="body2_nunito"
+              sx={{
+                "&:hover": {
+                  borderBottom: "3px solid black",
+                  paddingBottom: "16px",
+                },
+              }}
+            >
+              Boutiques
+            </Typography>
+          </Link>
+          <Link to="/public/partner_with_us">
+            <Typography
+              variant="body2_nunito"
+              sx={{
+                "&:hover": {
+                  borderBottom: "3px solid black",
+                  paddingBottom: "16px",
+                },
+              }}
+            >
+              Partner With Us
+            </Typography>
+          </Link>
+        </Stack>
+        <Stack
+          direction="row"
+          sx={{ position: "absolute", top: "20%", left: "46%" }}
+        >
+          <Box
+            component="img"
+            src={Logo}
+            alt="Stylit Logo"
+            sx={{ height: "28px" }}
+          />
+        </Stack>
+        <Stack
+          direction="row"
+          spacing={2.5}
           sx={{
-            position:'relative',
-            padding: "8px 25px",
             display: "flex",
-            justifyContent: "space-around",
+            justifyContent: "center",
             alignItems: "center",
+            marginLeft: "1100px",
           }}
         >
-          <Stack direction="row" spacing={5} sx={{position:'absolute' , top:"30%" , left:"8%"}}>
-          <Link to="/public/women">
-              <Typography
-                variant="body2_nunito"
-                sx={{
-                  "&:hover": {
-                    borderBottom: "3px solid black",
-                    paddingBottom: "16px",
-                  },
-                }}
-              >
-                Women
-              </Typography>
-            </Link>
-            <Link to="/public/men">
-              <Typography
-                variant="body2_nunito"
-                sx={{
-                  "&:hover": {
-                    borderBottom: "3px solid black",
-                    paddingBottom: "16px",
-                  },
-                }}
-              >
-                Men
-              </Typography>
-            </Link>
-            <Link to="#">
-              <Typography
-                variant="body2_nunito"
-                sx={{
-                  "&:hover": {
-                    borderBottom: "3px solid black",
-                    paddingBottom: "16px",
-                  },
-                }}
-              >
-                Boutiques
-              </Typography>
-            </Link>
-            <Link to="/public/partner_with_us">
-              <Typography
-                variant="body2_nunito"
-                sx={{
-                  "&:hover": {
-                    borderBottom: "3px solid black",
-                    paddingBottom: "16px",
-                  },
-                }}
-              >
-                Partner With Us
-              </Typography>
-            </Link>
-          </Stack>
-          <Stack direction="row" sx={{position:'absolute' , top:'20%' , left:"46%"}}>
-            <Box
-              component="img"
-              src={Logo}
-              alt="Stylit Logo"
-              sx={{ height: "28px"}}
-            />
-          </Stack>
-          <Stack
-            direction="row"
-            spacing={2}
+          <IconButton
             sx={{
+              borderRadius: "5px",
+              background: "#f0f0f0",
+              padding: "4px 4px", // Adjust padding as needed
               display: "flex",
               justifyContent: "center",
               alignItems: "center",
-              marginLeft:"900px"
             }}
+
+            onClick={navigateToSearchPage}
           >
-            <Stack direction="row">
-              <TextField
-                label="SEARCH"
-                size="small"
-                sx={{
-                  width: "300px",
-                  "& .MuiInputBase-input": {
-                    py: "5px",
-                  },
-                  boxShadow: "none",
-                  borderRadius: "0px",
-                }}
-                InputProps={{
-                  endAdornment: (
-                    <InputAdornment position="end">
-                      <SearchIcon />
-                    </InputAdornment>
-                  ),
-                }}
-                InputLabelProps={{
-                  style: { fontSize: "12px", textAlign: "center" }, // Adjust label font size here
-                }}
-              />
-            </Stack>
-            <ShopperSignUp/>
-            <Link to="#">
-              {/* <FavoriteBorderOutlinedIcon
+            <span className="material-symbols-outlined search">
+              search
+            </span>
+          </IconButton>
+
+          {/* <ShopperSignUp/> */}
+          <ProfileMenu />
+
+          <Link to="#">
+            {/* <FavoriteBorderOutlinedIcon
                 sx={{ fontSize: "30px", color: "rgba(0, 0, 0, 0.6)" }}
               /> */}
-              <span class="material-symbols-outlined topbar-icon">favorite</span>
-            </Link>
-            <Link to="#">
-              {/* <ShoppingBagOutlinedIcon
+            <span class="material-symbols-outlined topbar-icon1">favorite</span>
+          </Link>
+          <Link to="#">
+            {/* <ShoppingBagOutlinedIcon
                 sx={{ fontSize: "30px", color: "rgba(0, 0, 0, 0.6)" }}
               /> */}
-              <span class="material-symbols-outlined topbar-icon">local_mall</span>
-            </Link>
-          </Stack>
+            <span class="material-symbols-outlined topbar-icon1">
+              local_mall
+            </span>
+          </Link>
         </Stack>
       </Stack>
+    </Stack>
   );
 };
 
