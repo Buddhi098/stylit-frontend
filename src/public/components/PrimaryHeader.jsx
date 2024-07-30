a53393418918d83fc463314f60b235fbe530e822
Buddhi Nadeeshan <Buddhinadeeshan97@gmail.com>
2024-07-31 01:15:34 +0530

    update primary header

--- src/public/components/PrimaryHeader.jsx
+++ src/public/components/PrimaryHeader.jsx
@@ -6,19 +6,22 @@ import {
   InputAdornment,
   IconButton,
 } from "@mui/material";
-import React from "react";
+import React, { useContext } from "react";
 import Logo from "../assets/images/logo/Stylit_logo.png";
 import { Link, useNavigate } from "react-router-dom";
 import ProfileMenu from "./ProfileMenu";
 import "./primaryHeader.css";
 import Cart from "../../registered_shopper/pages/cart/Cart";
+import AuthContext from "../../context/auth_context/AuthContext";
 
 const PrimaryHeader = ({ topHeaderColor }) => {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
+
+  const { user } = useContext(AuthContext);
 
   const navigateToSearchPage = () => {
-    navigate('/public/search')
-  }
+    navigate("/public/search");
+  };
   return (
     <Stack direction="column" sx={{ borderBottom: "1px solid #f0f0f0" }}>
       <Box
@@ -81,19 +84,22 @@ const PrimaryHeader = ({ topHeaderColor }) => {
               Boutiques
             </Typography>
           </Link>
-          <Link to="/public/partner_with_us">
-            <Typography
-              variant="body2_nunito"
-              sx={{
-                "&:hover": {
-                  borderBottom: "3px solid black",
-                  paddingBottom: "16px",
-                },
-              }}
-            >
-              Partner With Us
-            </Typography>
-          </Link>
+
+          {!user?.resource_access.stylit.roles.includes("shopper") && (
+            <Link to="/public/partner_with_us">
+              <Typography
+                variant="body2_nunito"
+                sx={{
+                  "&:hover": {
+                    borderBottom: "3px solid black",
+                    paddingBottom: "16px",
+                  },
+                }}
+              >
+                Partner With Us
+              </Typography>
+            </Link>
+          )}
         </Stack>
         <Stack
           direction="row"
@@ -119,30 +125,32 @@ const PrimaryHeader = ({ topHeaderColor }) => {
           <IconButton
             sx={{
               borderRadius: "5px",
-              background: "#f0f0f0",
+              background: "#ffffff",
               padding: "4px 4px", // Adjust padding as needed
               display: "flex",
               justifyContent: "center",
               alignItems: "center",
             }}
-
             onClick={navigateToSearchPage}
           >
-            <span className="material-symbols-outlined search">
-              search
-            </span>
+            <span className="material-symbols-outlined search">search</span>
           </IconButton>
 
           {/* <ShopperSignUp/> */}
           <ProfileMenu />
 
-          <Link to="/shopper/favorite">
-            {/* <FavoriteBorderOutlinedIcon
+          {user?.resource_access.stylit.roles.includes("shopper") && (
+            <Link to="/shopper/favorite">
+              {/* <FavoriteBorderOutlinedIcon
                 sx={{ fontSize: "30px", color: "rgba(0, 0, 0, 0.6)" }}
               /> */}
-            <span class="material-symbols-outlined topbar-icon1">favorite</span>
-          </Link>
-          <Cart/>
+              <span class="material-symbols-outlined topbar-icon1">
+                favorite
+              </span>
+            </Link>
+          )}
+
+          {user?.resource_access.stylit.roles.includes("shopper") && <Cart />}
         </Stack>
       </Stack>
     </Stack>
