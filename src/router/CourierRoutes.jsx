c6d04331061093f7bc70e6ae647e3957be631e21
Shimra Hanan <shimrahanan09@gmail.com>
2024-07-31 11:14:09 +0530

    fashion UI updates

--- src/router/CourierRoutes.jsx
+++ src/router/CourierRoutes.jsx
@@ -1,25 +1,25 @@
-import { lazy } from 'react';
+// import { lazy } from 'react';
 
-// project import
-import Loadable from '../components/Loadable';
+// // project import
+// import Loadable from '../components/Loadable';
 
-import CourierLayout from '../courier/Courier';
-import { element } from 'prop-types';
+// import CourierLayout from '../courier/Courier';
+// import { element } from 'prop-types';
 
-const Dashboard = Loadable(lazy(()=>import('../courier/pages/dashboard')))
+// const Dashboard = Loadable(lazy(()=>import('../courier/pages/dashboard')))
 
 
-// ==============================|| MAIN ROUTING ||============================== //
-const CourierRoutes = {
-  path: '/courier',
-  element:<CourierLayout/>,
-  children: [
-    {
-      path: 'dashboard',
-      element: <Dashboard/>
-    },
+// // ==============================|| MAIN ROUTING ||============================== //
+// const CourierRoutes = {
+//   path: '/courier',
+//   element:<CourierLayout/>,
+//   children: [
+//     {
+//       path: 'dashboard',
+//       element: <Dashboard/>
+//     },
 
-  ]
-};
+//   ]
+// };
 
-export default CourierRoutes;
+// export default CourierRoutes;
