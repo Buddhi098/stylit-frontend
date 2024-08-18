8cb65695b7d9567376183be412f163dd939ad579
Buddhi Nadeeshan <Buddhinadeeshan97@gmail.com>
2024-08-18 14:33:52 +0530

    Web APi

---
+++ src/courier/api/WebApi.jsx
@@ -0,0 +1,21 @@
+// axiosInstance.js
+import axios from 'axios';
+
+const WebApi = axios.create({
+  baseURL: process.env.REACT_APP_BACKEND_BASE_URL,
+  timeout: 20000,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  withCredentials: false,
+});
+
+WebApi.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token');
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+export default WebApi;
