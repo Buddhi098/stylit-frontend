8142b7921a8f19863848b6971fbf4b063f377ec1
joannefdo <136670113+joannefdo@users.noreply.github.com>
2024-09-04 10:12:40 +0530

    added new update product component for the product update form

---
+++ src/shop/pages/product/update_new_products/UpdateProduct.jsx
@@ -0,0 +1,15 @@
+import { Stack } from '@mui/system'
+import React from 'react'
+import ProductForm from './ProductForm'
+import DataTable from './DataTable'
+
+
+const UpdateProduct = () => {
+  return (
+    <Stack>
+      <DataTable/>
+    </Stack>
+  )
+}
+
+export default UpdateProduct
