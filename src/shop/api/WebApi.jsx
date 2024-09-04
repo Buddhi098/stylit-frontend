2ad76a759e3ddfbc3a34ba20804be4dab7d637e4
joannefdo <136670113+joannefdo@users.noreply.github.com>
2024-09-04 14:26:38 +0530

    updated Webapi

--- src/shop/api/WebApi.jsx
+++ src/shop/api/WebApi.jsx
@@ -4,9 +4,6 @@ import axios from 'axios';
 const WebApi = axios.create({
   baseURL: process.env.REACT_APP_BACKEND_BASE_URL,
   timeout: 20000,
-  headers: {
-    'Content-Type': 'application/json',
-  },
   withCredentials: false,
 });
 
