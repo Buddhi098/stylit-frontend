7c0dda26518cab1116e0130806f30c9b4db9bd4e
joannefdo <136670113+joannefdo@users.noreply.github.com>
2024-09-04 10:57:12 +0530

    Update

--- src/shop/pages/product/update_new_products/UpdateProduct.jsx
+++ src/shop/pages/product/update_new_products/UpdateProduct.jsx
@@ -2,12 +2,18 @@ import { Stack } from '@mui/system'
 import React from 'react'
 import ProductForm from './ProductForm'
 import DataTable from './DataTable'
+import { useParams } from 'react-router'
 
 
 const UpdateProduct = () => {
+
+  const {id} = useParams()
+
+  console.log(id)
+
   return (
     <Stack>
-      <DataTable/>
+      <ProductForm/>
     </Stack>
   )
 }
