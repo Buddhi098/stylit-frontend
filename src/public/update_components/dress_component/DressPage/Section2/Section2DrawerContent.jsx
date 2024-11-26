ccb936f51224b99fc900338ab865670bbee1aa72
joannefdo <joannefdo01@gmail.com>
2024-11-26 16:09:51 +0530

    drawer content update

---
+++ src/public/update_components/dress_component/DressPage/Section2/Section2DrawerContent.jsx
@@ -0,0 +1,16 @@
+import { Grid, Stack } from "@mui/material";
+import React from "react";
+
+import { margin } from "@mui/system";
+import ProductImage from "./DrawerContent/ProductImage";
+import ProductDetails from "./DrawerContent/ProductDetails";
+const Section2DrawerContent = () => {
+  return (
+    <Stack direction="row" width={"96%"} sx={{margin:"40px auto"}}  minHeight={"600px"} spacing={2}>
+      <ProductImage/>
+      <ProductDetails/>
+    </Stack>
+  );
+};
+
+export default Section2DrawerContent;
